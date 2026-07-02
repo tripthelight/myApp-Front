93a9fa1f8c80bd2ee13f6368b0fc0661a8719abd
tripthelight <bye_ok@naver.com>
2026-07-03 02:19:31 +0900

    Use single automatic front deploy workflow

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -9,9 +9,15 @@ IMAGE_TAG="${IMAGE_TAG:-manual-$(date +%Y%m%d%H%M%S)}"
 
 cd "$PROJECT_DIR"
 
+echo "Front image build: $IMAGE_NAME:$IMAGE_TAG"
+
+echo "[1/3] Install dependencies and build Front"
 npm install
 npm run build
+
+echo "[2/3] Build Docker image"
 docker build --tag "$IMAGE_NAME:$IMAGE_TAG" .
 
+echo "[3/3] Deploy through Infra default.conf switcher"
 cd "$INFRA_DIR"
 IMAGE_OVERRIDE="$IMAGE_NAME:$IMAGE_TAG" ./scripts/deploy-front.sh
